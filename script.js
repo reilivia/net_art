5042d78ea9a375c0e91ace40e5d4dfea66809c10
Rebecca <rebeccaliviahamijaya@RLH.local>
2022-08-19 11:06:15 +1000

    update

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+document.body.style.margin = 0
+document.body.style.overflow = 'hidden'
+
 function setup () {
-    createCanvas (400, 400)
+    createCanvas (window.innerWidth, window.innerHeight)
+    rectMode (CENTER)
+    noStroke ()
+    fill ('hotpink')
 }
 
 function draw () {
     background (`pink`)
+    square (width / 2, height / 2, 100)
 }
